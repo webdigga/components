b5846f70b8721628784613aa669dc6abf9813de1
David White <webdigga42@gmail.com>
2016-10-09 20:44:59 +0100

    Remove Jasmine and install http://karma-runner.github.io/ #25

--- library/form-elements/scriptSpec.js
+++ library/form-elements/scriptSpec.js
@@ -1,14 +1,17 @@
-describe('Form elements', function() {
-    var Form = require(['script'], 'script'),
-        range;
+define(['library/form-elements/form'], function( form ) {
+	describe('Form elements', function() {
+		var fixture,
+			rangeValue;
 
-    beforeEach(function() {
-        //range = new Form();
-        rangeValue = 10;
-    });
+		beforeEach(function() {
+			fixture = '<div class="range"><input type="range" value="200" max="500" class="range__input js-range" /><div class="range__value js-range__value"></div></div>';
+			document.body.insertAdjacentHTML( 'afterbegin', fixture );
+		});
 
-    if('Should update the HTML with new value', function() {
-        Components.form.range();
-        expect(rangeValue.innerHTML).toEqual(10);
-    });
+		it('Should update the HTML with new value', function() {
+			document.querySelector( '.js-range' ).value = 10;
+			form.range();
+			expect(document.querySelector( '.js-range__value' ).innerHTML).toEqual('10');
+		});
+	});
 });
