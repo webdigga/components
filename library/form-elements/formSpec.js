e3c876a071779722f72f3e1491e4a88e0f7b97ae
David White <webdigga42@gmail.com>
2016-10-10 19:22:32 +0100

    File rename

--- library/form-elements/formSpec.js
+++ library/form-elements/formSpec.js
@@ -4,13 +4,23 @@ define(['library/form-elements/form'], function( form ) {
 			rangeValue;
 
 		beforeEach(function() {
+
+			// Create our HTML fixture
 			fixture = '<div class="range"><input type="range" value="200" max="500" class="range__input js-range" /><div class="range__value js-range__value"></div></div>';
+			
+			// Add it into the DOM
 			document.body.insertAdjacentHTML( 'afterbegin', fixture );
 		});
 
 		it('Should update the HTML with new value', function() {
+
+			// Set the valiue of the input field to be a value
 			document.querySelector( '.js-range' ).value = 10;
+
+			// Run the range method
 			form.range();
+
+			// Check that the HTML has been updated
 			expect(document.querySelector( '.js-range__value' ).innerHTML).toEqual('10');
 		});
 	});
